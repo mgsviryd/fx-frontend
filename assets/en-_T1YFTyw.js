a6a100e29cb3384fdfd3b7e27f5223001fbe8d0b
mgsviryd <mgsviryd@gmail.com>
2025-01-01 21:07:29 +0300

    Updates

---
+++ assets/en-_T1YFTyw.js
@@ -0,0 +1,1 @@
+const e={helloWorldMessage:"hello world is running",defaultTitle:"forex",chartTitle:"chart",mainTitle:"forex",nbrbTitle:"nbrb"};export{e as default};
---
+++ assets/ru--tN5pfN2.js
@@ -0,0 +1,1 @@
+const e={helloWorldMessage:"привет мир запущен",defaultTitle:"форекс",chartTitle:"график",mainTitle:"форекс",nbrbTitle:"нбрб"};export{e as default};
