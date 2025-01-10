c64bdc981fdf96d451edd648e9976d803b3b3f1a
mgsviryd <mgsviryd@gmail.com>
2025-01-10 17:17:36 +0200

    vite: clear console after reload

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// clear console after reload
+if (import.meta.hot) {
+    import.meta.hot.on(
+        "vite:beforeUpdate",
+        () => console.clear() // comment to disable
+    )
+}
+
 createApp(App).mount('#app')
