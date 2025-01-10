3b97ed6c3e74d8c9e7e6a9bcbcaa6b512454c01a
mgsviryd <mgsviryd@gmail.com>
2025-01-10 20:28:54 +0200

    vue: .vue file structure (...)
    Structure:
      <template> - visual content
      <script> - data and method which work with content
      <style scoped> - css styles (scoped means apply only to here, not other vue files)
    
    Inside template can be only one tag section, here <div>. Good practice to set it id.
    
    :id="ids.id" - set property id using computed value ids (Ctrl/Command + click - to see where declared value). Set any property in this manner.
    
    props:
      - like constructor in java class, for example:
        <templ :name="Pasha"></templ> - it's an instance of Templ.vue where props: ['name'] - it's a constructor
    
    components:
      - list names of imported vue files which instances we want to inject in template, for example:
        components:{Templ1, Templ2} looks like
          <template>
            <div>
             <templ1></templ1> // inject
             <templ2></templ2> // inject
            </div>
          </template>
    
    created:
      stage of execution before rendering, here you can call methods or other js, here we call this.fetchData() method using this as reference to this vue instance
    
    computed:
      it's a section when we can declare automatically updated variables when computed variable or variable of data section or props is changed
    
    watch:
      it's a section when we catch change events over computed variable or variable of data section or props (immediate: true - means compare with version before created after object will be created, deep: true - means track all properties inside object)
    
    data:
      as properties in java class
    
    methods:
      as methods in java class

---
+++ src/components/Templ.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :id="ids.id" class="">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: [],
+  components: {},
+  async created() {
+    await this.fetchData()
+  },
+  computed: {
+    prefix() {
+      return this.id + '-' + this.name + '-'
+    },
+    ids() {
+      return {
+        id: this.prefix,
+      }
+    },
+  },
+  watch: {
+    '$route': {
+      handler(to, from) {
+        this.fetchData()
+      },
+      immediate: true,
+    },
+  },
+  data() {
+    return {
+      name: "Templ",
+    }
+  },
+  methods: {
+    fetchData() {
+
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
