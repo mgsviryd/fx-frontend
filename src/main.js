61309e344af0495e1576b830e8df43fe1c7dca37
mgsviryd <mgsviryd@gmail.com>
2025-01-10 19:08:58 +0200

    css: add Bootstrap-vue-next (...)
    https://bootstrap-vue-next.github.io/bootstrap-vue-next/docs.html
    
    Setup:
      $ npm i unplugin-vue-components@0.28.0 --save-dev
      (i means 'install', --save-dev means the installed component won't be installed in production)
      $ npm i bootstrap-vue-next@0.26.18
    
    Notice:
      - packages in package.json you can load manually on machine by command (as example, in case if you delete node_modules folder):
        $ npm install
          - for dev
        $ npm install --production
          - for production
    
      - {{'Hello world'}} - use {{}} as  vue syntax to inject in vue value or variable

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 import $ from 'jquery'
+import {createBootstrap} from 'bootstrap-vue-next'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 
 window.jQuery = window.$ = $
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [BootstrapVueNextResolver()],
+        }),
+    ],
 
-  server: {
-    port: 8081,
-    cors: true,
-    hot: true,
-    proxy: {
-      'api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        secure: false,
-      },
-    }
-  },
+    server: {
+        port: 8081,
+        cors: true,
+        hot: true,
+        proxy: {
+            'api': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                secure: false,
+            },
+        }
+    },
 })
