513fafbad3603943cdcd46b4fd04588f63200081
mgsviryd <mgsviryd@gmail.com>
2025-01-10 16:56:39 +0200

    vite: set server (...)
    cors:
      - https://doka.guide/tools/cors/ - see here
    
    hot:
      - reload page after change automatically
    
    proxy:
      - https://vite.dev/config/server-options#server-proxy
      - set url mapping with: host, port, its settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  server: {
+    port: 8081,
+    cors: true,
+    hot: true,
+    proxy: {
+      'api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  },
 })
