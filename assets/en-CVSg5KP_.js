24c007a7209b6474dbaeec0128ee8fb95cd4eaa2
mgsviryd <mgsviryd@gmail.com>
2025-01-12 20:35:17 +0300

    Updates

---
+++ assets/en-CVSg5KP_.js
@@ -0,0 +1,1 @@
+const l={helloWorld:"hello world",defaultTitle:"forex",mainTitle:"hello world title"};export{l as default};
---
+++ assets/ru-h-w8cQyM.js
@@ -0,0 +1,1 @@
+const l={helloWorld:"привет мир",defaultTitle:"форекс",mainTitle:"привет мир заголовок"};export{l as default};
