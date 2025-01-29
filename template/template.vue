a0e6ee4f327e5f47c01c56bba0671a6695ddc125
mgsviryd <mgsviryd@gmail.com>
2025-01-29 13:55:20 +0300

    intellij: file template .vue

---
+++ template/template.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :id="ids.id" class="">
+
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+import * as _ from 'lodash'
+
+export default {
+  props: {},
+  components: {},
+  async created() {
+    await this.fetchData()
+  },
+  computed: {
+    ...mapState([
+      'lang',
+    ]),
+    ...mapGetters([
+
+    ]),
+    prefix() {
+      return this.id + '-' + this.name + '-'
+    },
+    ids() {
+      return {
+        id: this.prefix,
+      }
+    },
+  },
+  watch: {
+    '#[[$]]#route': {
+      handler(to, from) {
+        this.fetchData()
+      },
+      immediate: true,
+    },
+  },
+  data() {
+    return {
+      name: "${COMPONENT_NAME}",
+    }
+  },
+  methods: {
+    fetchData() {
+
+    },
+    getCapitalizeLang(key) {
+      return _.capitalize(this.getLang(key))
+    },
+    getLang(key) {
+      return this.#[[$]]#t(key)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
